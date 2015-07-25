be485377cfbae8c7ecfd3b676a8fef57696e023b
Sofia Nguy <sofianguy@gmail.com>
2015-07-24 17:32:03 -0700

    added more comment notes to index.js file

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var http = require("http"); //node documentation, http
+//http is built into node
+//
 var cool = require("cool-ascii-faces");
-console.log(cool())
 //createServer from http(piece of node library)
+//require is import equivalent in Python
+//so if i use "cool" it will take "cool" from cool-ascii-faces module
+
 //creates a server
-var server = http.createServer().listen(5000, 'localhost'); 
+var server = http.createServer() //creating server object; http server
+
 server.on("request", function(request, response) {
-	console.log("hello world"); //prints when there's a request; until user goes to localhost
-	response.write("<h1>random face</h1>" + "<p>"+ cool() + "</p>"); //puts face inside <p>
+//"request" means each time web browser wants to connect
+//function(request, response) --call back, 	
+	//request is what browser sends to terminal
+	//response is how you send things from terminal back to web browser
+
 	response.write(cool()); //shows up on localhost
-	response.end();
+	//.write() writes to browser
+	//can write code, e.g., html
+	//console.log into the web browser
+	response.end();//if you don't have .end(), then the browser will keep running. 
+	console.log(request.url) //if request.url === do something; similar to Flask. 
+	//express module will do this. so you don't put if statements.
 });
+
+server.listen(5000, 'localhost'); //name of server
+//if do not specify port number, default is 80
