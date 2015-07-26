158dfd075e589bacc67f0494a08a83b59995562f
Sofia Nguy <sofianguy@gmail.com>
2015-07-25 23:34:46 -0700

    added more comment notes for better understanding of program

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var http = require("http"); //node documentation, http
 //http is built into node
-//
+//require is a built-in node function
+
 var cool = require("cool-ascii-faces");
 //createServer from http(piece of node library)
-//require is import equivalent in Python
-//so if i use "cool" it will take "cool" from cool-ascii-faces module
+//require is like import in Python
+//so if I use variable cool, it will take variable cool from 
+// cool-ascii-faces module
 
-//creates a server
 var server = http.createServer() //creating server object; http server
 
 server.on("request", function(request, response) {
@@ -15,14 +16,15 @@ server.on("request", function(request, response) {
 	//request is what browser sends to terminal
 	//response is how you send things from terminal back to web browser
 
-	response.write(cool()); //shows up on localhost
-	//.write() writes to browser
-	//can write code, e.g., html
-	//console.log into the web browser
+	response.write(cool()); //shows up on localhost server
+	//.write() writes to the browser
+	//can also write code inside (), e.g., html code
+
 	response.end();//if you don't have .end(), then the browser will keep running. 
 	console.log(request.url) //if request.url === do something; similar to Flask. 
-	//express module will do this. so you don't put if statements.
+	//Express module will do this. 
+	//So you don't put if statements.
 });
 
-server.listen(5000, 'localhost'); //name of server
-//if do not specify port number, default is 80
+server.listen(5000, 'localhost'); //name of server "localhost:5000"
+//if port number is not specified, default is 80
